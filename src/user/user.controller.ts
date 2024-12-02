f28b35a78dde915e07e99457ffa19420df7ccea7
mariammahamat <mariammahamat557@gmail.com>
2024-12-02 15:21:14 +0100

    modification de user et tags

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(+id);
+    }
+
+    @Post()
+    create(@Body() user: { name: string; last_name: string ; email: string; password: string; role: string;}) {
+        return this.userService.create(user);
+    }
+    
+    // @Put(':id')
+    // update(
+    //     @Param('id') id: string,
+    //     @Body() updatedUser: Partial<{ name: string; prenom: string; email: string; password: string; role: string }>,
+    // ){
+    //     return this.userService.update(+id, updatedUser);
+    // }
+
+    @Patch()
+    update(
+        @Query('id') id: string,
+        @Body() updatedUser: { name?: string; last_name?: string; email?: string; password?: string; role?: string },
+    ) {
+        return this.userService.update(+id, updatedUser);
+    }
+
+    // @Delete(':id')
+    // delete(@Param('id') id: string) {
+    //     return this.userService.delete(+id);
+    // }
+
+    @Delete()
+    delete(@Query('id') id: string) {
+      return this.userService.delete(+id);
+    }
+}
+
+
+
+
