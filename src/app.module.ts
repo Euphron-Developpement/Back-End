7205e55536b3b1747cf70079a34080ba2b7a09ea
quentinmel <qmeleo@gmail.com>
2024-12-02 11:05:24 +0100

    feat: get article & delete routing module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoutingModule } from './routing/routing.module';
 import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 import { MediaModule } from './media/media.module';
 import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [RoutingModule, UserModule, ArticleModule, MediaModule, TagsModule],
+  imports: [UserModule, ArticleModule, MediaModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
