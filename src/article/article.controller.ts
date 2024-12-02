139e225cc3f642d287c607d3b9b243c4ea4d42bb
quentinmel <qmeleo@gmail.com>
2024-12-02 11:04:58 +0100

    feat: get article & delete routing module

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import { ArticleService } from './article.service';
 
 @Controller('article')
-export class ArticleController {}
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Get()
+  findAll() {
+    return this.articleService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articleService.findOne(+id);
+  }
+}
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  private articles = [
+    { id: 1, title: 'Article 1', readTime: 5, categoryId: 1, publicationDate: new Date() },
+    { id: 2, title: 'Article 2', readTime: 10, categoryId: 2, publicationDate: new Date() },
+  ];
+
+  findAll() {
+    return this.articles;
+  }
+
+  findOne(id: number) {
+    return this.articles.find((article) => article.id === id);
+  }
+}
