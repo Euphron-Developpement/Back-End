30060d37932a62280f61a3ddc6a2487e45d453eb
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2024-12-02 15:56:34 +0100

    modification de media

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Delete, Patch, Query } from '@nestjs/common';
+import { MediaService } from './media.service';
 
 @Controller('media')
-export class MediaController {}
+export class MediaController {
+    constructor(private readonly mediaService: MediaService) {};
+
+    //Récupération de tous les medias
+    @Get()
+    findAll() {
+        return this.mediaService.findAll();
+    }
+
+    //Récupération d'un media
+    @Get(':id')
+    findOne(@Param('id') _id: number) {
+        return this.mediaService.findOne(+_id);
+    }
+
+    //Ajout d'un media
+    @Post()
+    create(@Body() media: { url: string; article_id: number; hero: boolean; type: number; }) {
+        return this.mediaService.create(media);
+    }
+
+    //Modification d'un media
+    @Patch()
+    update(
+        @Query('id') id: string,
+        @Body() updatedMedia: { url?: string; article_id?: number; hero?: boolean; type?: number; }
+    ) {
+        return this.mediaService.update(+id, updatedMedia);
+    }
+    
+    //Suppresion d'un media
+    @Delete()
+    delete(@Query('id') id: string) {
+        return this.mediaService.delete(+id);
+    }
+}
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { media } from '../interfaces/media.interface';
 
 @Injectable()
-export class MediaService {}
+export class MediaService {
+
+    private medias: media[] = [
+        {
+            id: 1, 
+            url: "https://docs.nestjs.com/controllers#routing",
+            article_id: 2,
+            hero: true,
+            type: 1,
+        },
+        {
+            id: 2, 
+            url: "https://docs.nestjs.com/controllers#routing",
+            article_id: 3,
+            hero: false,
+            type: 2,
+        }
+    ];
+
+    private currentId = 3;
+
+    //Récupération de tous les medias
+    findAll() {
+        return this.medias;
+    }
+
+    //Récupération d'un media
+    findOne(_id: number) {
+        return this.medias.find((medias) => medias.id === _id);
+    }
+
+    //Création d'un nouveau media
+    create(media: { url: string; article_id: number ; hero: boolean; type: number;}) {
+        const newMedia = { id: this.currentId++, ...media }; // Ajout d'un ID unique
+        this.medias.push(newMedia);
+        return newMedia;
+    }
+
+    //Suppresion d'un media
+    delete(id: number) {
+        const mediaIndex = this.medias.findIndex((media) => media.id === id);
+        if (mediaIndex === -1) {
+            return { error: `Media with ID ${id} not found` };
+        }
+    
+        const deletedMedia = this.medias.splice(mediaIndex, 1);
+        return deletedMedia[0];
+    }
+
+    //Modification d'un media
+    update(id: number, updatedMedia: Partial<{ url: string; article_id: number; hero: boolean; type: number; }>) {
+        const mediaIndex = this.medias.findIndex((media) => media.id === id);
+        if (mediaIndex === -1) {
+            return { error: `Media with ID ${id} not found` };
+        }
+    
+        this.medias[mediaIndex] = { ...this.medias[mediaIndex], ...updatedMedia };
+        return this.medias[mediaIndex];
+    }
+}
