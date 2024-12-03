30f2e53bfc6986e96f16f77a9c0435133baf8423
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2024-12-03 14:57:03 +0100

    Commit apres pull de la branche dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,11 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 import { TagsModule } from './tags/tags.module';
-<<<<<<< HEAD
-import { MediaModule } from './media/media.module';
-=======
 import { PrismaService } from './prisma/prisma.service';
->>>>>>> 040dbafbcd2f888fd56b2a122c69ef3df042199e
+import { MediaModule } from './media/media.module';
 
 @Module({
-  imports: [UserModule, ArticleModule, MediaModule, TagsModule, PrismaModule],
+  imports: [UserModule, ArticleModule, MediaModule, TagsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
