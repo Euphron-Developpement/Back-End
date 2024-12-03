fa12b2f53c0c5040808f48bcd0cd6c991d47b562
mariammahamat <mariammahamat557@gmail.com>
2024-12-03 14:42:27 +0100

    modification apporté

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 import { MediaModule } from './media/media.module';
 import { TagsModule } from './tags/tags.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UserModule, ArticleModule, MediaModule, TagsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
