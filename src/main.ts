8b7ffeae54d313cc2a686107b5f52b99db6118a3
Alexis <velris.dev@gmail.com>
2025-01-07 16:13:44 +0100

    Divers modification

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -9,6 +10,19 @@ async function bootstrap() {
       logger: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Euphron API")
+    .setDescription("API of Euphron project")
+    .setVersion("1.0")
+    .addTag("Euphron")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen({
     port: 3000,
     host: '0.0.0.0',
