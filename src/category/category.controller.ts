259e60d89da2650469b52db7678f5fb0abfe24bc
mariammahamat <mariammahamat557@gmail.com>
2025-01-08 12:18:42 +0100

    modifier pour utiliser la commande patch

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,55 +1,58 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Param,
-    Body,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { CategoryService } from './category.service';
-  import { Prisma } from '@prisma/client';
-  
-  @Controller('categories') // Base path for the routes
-  export class CategoryController {
-    constructor(private readonly categoryService: CategoryService) {}
-  
-    @Get()
-    async findAll() {
-      return this.categoryService.findAll();
-    }
-  
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-      const category = await this.categoryService.findOne(Number(id));
-      if (!category) {
-        throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
-      }
-      return category;
-    }
-  
-    @Post()
-    async create(@Body() data: Prisma.CategoryCreateInput) {
-      return this.categoryService.create(data);
-    }
-  
-    @Put(':id')
-    async update(
-      @Param('id') id: string,
-      @Body() updatedData: Prisma.CategoryUpdateInput,
-    ) {
-      return this.categoryService.update(Number(id), updatedData);
-    }
-  
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-      const category = await this.categoryService.delete(Number(id));
-      if (!category) {
-        throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
-      }
-      return category;
-    }
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('categories') // Base path for the routes
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async findAll() {
+    return this.categoryService.findAll();
   }
-  
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const category = await this.categoryService.findOne(Number(id));
+    if (!category) {
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+    }
+    return category;
+  }
+
+  @Post()
+  async create(@Body() data: Prisma.CategoryCreateInput) {
+    return this.categoryService.create(data);
+  }
+
+  @Patch(':id') // Remplacement de PUT par PATCH
+  async update(
+    @Param('id') id: string,
+    @Body() updatedData: Prisma.CategoryUpdateInput,
+  ) {
+    const category = await this.categoryService.update(Number(id), updatedData);
+    if (!category) {
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+    }
+    return category;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const category = await this.categoryService.delete(Number(id));
+    if (!category) {
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+    }
+    return category;
+  }
+}
--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -1,55 +1,61 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Param,
-    Body,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { ReservationService } from './reservation.service';
-  import { Prisma } from '@prisma/client';
-  
-  @Controller('reservations') // Base path for the routes
-  export class ReservationController {
-    constructor(private readonly reservationService: ReservationService) {}
-  
-    @Get()
-    async findAll() {
-      return this.reservationService.findAll();
-    }
-  
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-      const reservation = await this.reservationService.findOne(Number(id));
-      if (!reservation) {
-        throw new HttpException('Reservation not found', HttpStatus.NOT_FOUND);
-      }
-      return reservation;
-    }
-  
-    @Post()
-    async create(@Body() data: Prisma.ReservationCreateInput) {
-      return this.reservationService.create(data);
-    }
-  
-    @Put(':id')
-    async update(
-      @Param('id') id: string,
-      @Body() updatedData: Prisma.ReservationUpdateInput,
-    ) {
-      return this.reservationService.update(Number(id), updatedData);
-    }
-  
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-      const reservation = await this.reservationService.delete(Number(id));
-      if (!reservation) {
-        throw new HttpException('Reservation not found', HttpStatus.NOT_FOUND);
-      }
-      return reservation;
-    }
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ReservationService } from './reservation.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('reservations') // Base path for the routes
+export class ReservationController {
+  constructor(private readonly reservationService: ReservationService) {}
+
+  @Get()
+  async findAll() {
+    return this.reservationService.findAll();
   }
-  
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const reservation = await this.reservationService.findOne(Number(id));
+    if (!reservation) {
+      throw new HttpException('Reservation not found', HttpStatus.NOT_FOUND);
+    }
+    return reservation;
+  }
+
+  @Post()
+  async create(@Body() data: Prisma.ReservationCreateInput) {
+    return this.reservationService.create(data);
+  }
+
+  @Patch(':id') // Utilisation de PATCH pour les mises à jour partielles
+  async update(
+    @Param('id') id: string,
+    @Body() updatedData: Prisma.ReservationUpdateInput,
+  ) {
+    const reservation = await this.reservationService.update(
+      Number(id),
+      updatedData,
+    );
+    if (!reservation) {
+      throw new HttpException('Reservation not found', HttpStatus.NOT_FOUND);
+    }
+    return reservation;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const reservation = await this.reservationService.delete(Number(id));
+    if (!reservation) {
+      throw new HttpException('Reservation not found', HttpStatus.NOT_FOUND);
+    }
+    return reservation;
+  }
+}
